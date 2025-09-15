80752ca073abed7800b1dceee52d5355fd6be8ad
Spyro7883 <spinu11_2003@yahoo.ro>
2025-09-15 11:24:05 +0300

    variables, view functions, reward logic

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -2,29 +2,96 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Stacking{
-    mapping(address => uint256) public stakes;
-    mapping(address => uint256) public rewards;
-    uint256 public totalStaked;
-    uint256 public rewardRate;
+contract Staking{
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public rewardsToken;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public rewardsDuration = 7 days;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;    
+
+    mapping(address => uint256) private _balances;
+
+    uint256 private _totalSupply;
+
+    /* ========== VIEWS ========== */
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply)
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+    }
+    
+    /* ========== SPECIFIC FUNCTIONS ========== */
   
-    function stake(uint256 amount) public{
-        require(_amount > 0, "Amount must be greater than zero");
-        require(token.balanceOf(msg.sender) >= amount, "Insufficient balance");
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
-        stakes[msg.sender] += amount;
-        totalStaked += amount;
+    function stake(address token, uint256 amount) external updateReward(msg.sender) {
+        require(amount > 0, "Amount must be greater than zero");        
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        _totalSupply=_totalSupply.add(amount);
+        _balances[msg.sender] += amount;
         emit Staked(msg.sender, amount);
     }
-    function unstake(uint256 amount) public{
-        require(stakes[msg.sender] >= amount, "Insufficient stake");
-        stakes[msg.sender]-=amount;
-        totalStaked-=amount;
-        IERC20(token).transfer(msg.sender, amount);
-        emit Unstaked(msg.sender, amount);
+
+    function withdraw(address token, uint256 amount) external updateReward(msg.sender) {
+        require(_balances[msg.sender] >= amount, "Insufficient stake");
+        _balances[msg.sender]=_balances[msg.sender].sub(amount);
+        _totalSupply=_totalSupply.sub(amount);
+        IERC20(token).safeTransfer(msg.sender, amount);
+        emit Withdraw(msg.sender, amount);
     }
-    function calculateRewards(uint256 rewards) public{
-        
+
+    function getReward() public updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
     }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
 }
